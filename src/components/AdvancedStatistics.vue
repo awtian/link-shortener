a9f10327e872f478133df07fa48fdda16a6a286a
Awtian <me@awtian.com>
2023-02-09 21:17:57 +0700

    attempts on vertical line

--- src/components/AdvancedStatistics.vue
+++ src/components/AdvancedStatistics.vue
@@ -5,8 +5,43 @@
       Track how your links are performing across the web with our advanced
       statistics dashboard.
     </p>
+    <FeatureCard
+      title="Brand Recognition"
+      description="Boost your brand recognition with each click. Generic links don't mean a
+      thing. Branded links help instil confidence in your content."
+    >
+      <template #icon>
+        <img src="@/assets/images/icon-brand-recognition.svg" />
+      </template>
+    </FeatureCard>
+    <FeatureCard
+      title="Detailed Records"
+      description="Gain insight into who is clicking your links. Knowing how and where people engage with your content helps inform better decisions."
+      :verticalLine="true"
+    >
+      <template #icon>
+        <img src="@/assets/images/icon-detailed-records.svg" />
+      </template>
+    </FeatureCard>
+    <FeatureCard
+      title="Fully Customizable"
+      description="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+      :verticalLine="true"
+    >
+      <template #icon>
+        <img src="@/assets/images/icon-fully-customizable.svg" />
+      </template>
+    </FeatureCard>
   </div>
 </template>
+
+<script>
+import FeatureCard from "./advancedstatistics/FeatureCard.vue";
+
+export default {
+  components: { FeatureCard },
+};
+</script>
 
 <style scoped>
 .container {
@@ -25,5 +60,6 @@
   font-size: 14px;
   color: var(--color-neutral-gray);
   margin: 0px 20px;
+  margin-bottom: 100px;
 }
 </style>
---
+++ src/components/advancedstatistics/FeatureCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card-container">
+    <!-- <div v-if="verticalLine" class="vl"></div> -->
+    <div class="circle">
+      <slot name="icon"> </slot>
+    </div>
+    <h3 class="title">{{ title }}</h3>
+    <p class="description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { required: true, type: String },
+    description: { required: true, type: String },
+    verticalLine: { required: true, type: Boolean },
+  },
+};
+</script>
+
+<style scoped>
+.card-container {
+  background-color: white;
+  text-align: center;
+  margin: 0px 20px;
+  margin-top: -20px;
+  border-radius: 10px;
+  margin-bottom: 80px;
+  padding-bottom: 20px;
+}
+
+.title {
+  margin-top: -20px;
+}
+.description {
+  padding: 0px 15px;
+  color: var(--color-neutral-gray);
+  font-size: 14px;
+}
+.circle {
+  position: relative;
+  top: -40px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 75px;
+  height: 75px;
+  background-color: var(--color-primary-dark-violet);
+}
+.vl {
+  border-left: 6px solid green;
+  height: 100px;
+  position: relative;
+  left: 50%;
+  /* top: 10px; */
+  /* top: 1%; */
+}
+</style>
