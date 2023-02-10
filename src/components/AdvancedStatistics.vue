77976f375e66a98df9731fffe310a6aee7834a4c
Awtian <me@awtian.com>
2023-02-10 09:41:28 +0700

    desktop advanced statistic

--- src/components/AdvancedStatistics.vue
+++ src/components/AdvancedStatistics.vue
@@ -2,36 +2,42 @@
   <div class="container">
     <h2>Advanced Statistics</h2>
     <p class="subtitle">
-      Track how your links are performing across the web with our advanced
-      statistics dashboard.
+      Track how your links are performing across the web with <br />
+      our advanced statistics dashboard.
     </p>
-    <FeatureCard
-      title="Brand Recognition"
-      description="Boost your brand recognition with each click. Generic links don't mean a
-      thing. Branded links help instil confidence in your content."
-    >
-      <template #icon>
-        <img src="@/assets/images/icon-brand-recognition.svg" />
-      </template>
-    </FeatureCard>
-    <FeatureCard
-      title="Detailed Records"
-      description="Gain insight into who is clicking your links. Knowing how and where people engage with your content helps inform better decisions."
-      :verticalLine="true"
-    >
-      <template #icon>
-        <img src="@/assets/images/icon-detailed-records.svg" />
-      </template>
-    </FeatureCard>
-    <FeatureCard
-      title="Fully Customizable"
-      description="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
-      :verticalLine="true"
-    >
-      <template #icon>
-        <img src="@/assets/images/icon-fully-customizable.svg" />
-      </template>
-    </FeatureCard>
+    <hr class="horizontal-line" />
+    <div class="feature-card-container">
+      <FeatureCard
+        title="Brand Recognition"
+        description="Boost your brand recognition with each click. Generic links don't mean a
+        thing. Branded links help instil confidence in your content."
+        topgap="0"
+      >
+        <template #icon>
+          <img src="@/assets/images/icon-brand-recognition.svg" />
+        </template>
+      </FeatureCard>
+      <FeatureCard
+        title="Detailed Records"
+        description="Gain insight into who is clicking your links. Knowing how and where people engage with your content helps inform better decisions."
+        :line="true"
+        topgap="1"
+      >
+        <template #icon>
+          <img src="@/assets/images/icon-detailed-records.svg" />
+        </template>
+      </FeatureCard>
+      <FeatureCard
+        title="Fully Customizable"
+        description="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+        :line="true"
+        topgap="2"
+      >
+        <template #icon>
+          <img src="@/assets/images/icon-fully-customizable.svg" />
+        </template>
+      </FeatureCard>
+    </div>
   </div>
 </template>
 
@@ -61,5 +67,32 @@ export default {
   color: var(--color-neutral-gray);
   margin: 0px 20px;
   margin-bottom: 100px;
+}
+
+.horizontal-line {
+  display: none;
+}
+
+@media screen and (min-width: 426px) {
+  .feature-card-container {
+    padding: 0px 100px;
+    display: flex;
+  }
+
+  .container {
+    text-align: center;
+  }
+
+  .container h2 {
+    font-size: 36px;
+  }
+
+  .horizontal-line {
+    position: relative;
+    border: 5px solid var(--color-primary-cyan);
+    top: 175px;
+    display: block;
+    margin: 0px 120px;
+  }
 }
 </style>
--- src/components/advancedstatistics/FeatureCard.vue
+++ src/components/advancedstatistics/FeatureCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="card-container">
+  <div class="card-container" :class="cardContainerClass">
     <!-- <div v-if="verticalLine" class="vl"></div> -->
-    <div class="circle" :class="{ vl: verticalLine }">
+    <div class="circle" :class="{ vl: line }">
       <slot name="icon"> </slot>
     </div>
     <h3 class="title">{{ title }}</h3>
@@ -16,18 +16,98 @@ export default {
   props: {
     title: { required: true, type: String },
     description: { required: true, type: String },
-    verticalLine: { required: true, type: Boolean },
+    line: { default: false, type: Boolean },
+    topgap: { default: 0, type: Number },
+  },
+  computed: {
+    cardContainerClass() {
+      return {
+        [`gap-${this.topgap}`]: true,
+      };
+    },
   },
 };
 </script>
 
 <style scoped>
+@media screen and (min-width: 426px) {
+  .card-container {
+    width: 30%;
+    height: 200px;
+    text-align: left !important;
+    padding: 100px 10px 0px 10px;
+    margin: 0px 15px;
+  }
+
+  .title {
+    /* margin-top: -20px; */
+    margin-left: 1em;
+  }
+
+  .description {
+    font-size: 16px;
+  }
+  .circle {
+    position: absolute;
+    top: -37px;
+    left: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 75px;
+    height: 75px;
+    background-color: var(--color-primary-dark-violet);
+  }
+
+  .gap-0 {
+    margin-top: 0px;
+  }
+  .gap-1 {
+    margin-top: 50px;
+  }
+
+  .gap-2 {
+    margin-top: 100px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .vl:before {
+    content: "";
+    position: absolute;
+    height: 50px;
+    border-right: 6px solid var(--color-primary-cyan);
+    top: 0;
+    margin-top: -40px;
+    z-index: -1;
+  }
+
+  .circle {
+    position: relative;
+    top: -40px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 75px;
+    height: 75px;
+    background-color: var(--color-primary-dark-violet);
+  }
+  .description {
+    font-size: 14px;
+  }
+
+  .card-container {
+    margin-top: 0px;
+    margin: 0px 15px;
+  }
+}
 .card-container {
   position: relative;
   background-color: white;
   text-align: center;
-  margin: 0px 15px;
-  margin-top: 0px;
   border-radius: 10px;
   margin-bottom: 80px;
   padding-bottom: 20px;
@@ -40,28 +120,5 @@ export default {
 .description {
   padding: 0px 25px;
   color: var(--color-neutral-gray);
-  font-size: 14px;
-}
-.circle {
-  position: relative;
-  top: -40px;
-  margin: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  width: 75px;
-  height: 75px;
-  background-color: var(--color-primary-dark-violet);
-}
-
-.vl:before {
-  content: "";
-  position: absolute;
-  height: 50px;
-  border-right: 6px solid var(--color-primary-cyan);
-  top: 0;
-  margin-top: -40px;
-  z-index: -1;
 }
 </style>
