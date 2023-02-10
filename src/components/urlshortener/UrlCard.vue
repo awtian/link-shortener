c4c8e715b196b21e8df5a8d727083e1a48864f08
Awtian <me@awtian.com>
2023-02-10 08:20:00 +0700

    desktop url card

--- src/components/urlshortener/UrlCard.vue
+++ src/components/urlshortener/UrlCard.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="url-card">
     <p>{{ conciseLongUrl }}</p>
-    <hr />
-    <a class="short-url" :href="shortUrl" target="_blank">{{ shortUrl }}</a>
-    <div class="button-container">
-      <button :class="buttonClass" @click="handleCopyUrl">{{ text }}</button>
+    <hr class="splitter" />
+    <div class="url-button-container">
+      <a class="short-url" :href="shortUrl" target="_blank">{{ shortUrl }}</a>
+      <div class="button-container">
+        <button :class="buttonClass" @click="handleCopyUrl">{{ text }}</button>
+      </div>
     </div>
   </div>
 </template>
@@ -55,7 +57,23 @@ export default {
   margin: 10px;
   border-radius: 10px;
 }
+.url-card button {
+  width: calc(100% - 20px);
+  padding: 10px;
+  border-width: 0;
+  margin: 10px 5px 15px 5px;
+  font-family: "Poppins";
+  border-radius: 5px;
+  color: white;
+  font-weight: 700;
+}
+.button-idle {
+  background-color: var(--color-primary-cyan);
+}
 
+.button-clicked {
+  background-color: var(--color-primary-dark-violet);
+}
 .url-card p,
 .url-card a {
   padding: 10px;
@@ -68,37 +86,50 @@ export default {
   text-decoration: underline;
 }
 
-.url-card hr {
-  padding: 0;
-  margin: 0;
-  color: var(--color-neutral-gray);
-  opacity: 0.1;
-}
-
-.button-container {
-  text-align: center;
-}
-
 .short-url {
-  padding-bottom: 0 !important;
   color: var(--color-primary-cyan);
 }
-.url-card button {
-  width: calc(100% - 20px);
-  padding: 10px;
-  border-width: 0;
-  margin: 10px 5px 15px 5px;
-  font-family: "Poppins";
-  border-radius: 5px;
-  color: white;
-  font-weight: 700;
+
+@media screen and (max-width: 425px) {
+  .url-card hr {
+    padding: 0;
+    margin: 0;
+    color: var(--color-neutral-gray);
+    opacity: 0.1;
+  }
+
+  .short-url {
+    padding-bottom: 0 !important;
+  }
+  .button-container {
+    text-align: center;
+  }
 }
 
-.button-idle {
-  background-color: var(--color-primary-cyan);
-}
+@media screen and (min-width: 426px) {
+  .url-card {
+    display: flex;
+    height: 75px;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px 0;
+    padding: 0px 20px;
+    border-radius: 5px;
+  }
 
-.button-clicked {
-  background-color: var(--color-primary-dark-violet);
+  .url-button-container {
+    display: flex;
+    gap: 20px;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .url-button-container button {
+    width: 150px;
+    cursor: pointer;
+  }
+  .splitter {
+    display: none;
+  }
 }
 </style>
